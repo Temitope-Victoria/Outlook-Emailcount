3f2491c8260345f90a9b9478366b2b8389249810
Victoria Temitope <57282172+Temitope-Victoria@users.noreply.github.com>
2023-05-17 21:29:39 +0100

    Emailcount

---
+++ unread emails count.ps1
@@ -0,0 +1,10 @@
+﻿Get-MailboxFolderStatistics temitope.opeagbe@funmilayo.tk | Select Name,FolderSize,ItemsinFolder
+
+$outlook = new-object -com Outlook.Application
+$session = $outlook.Session
+$session.Logon()
+$inbox = $outlook.session.GetDefaultFolder(6)
+[array]$unreadCount = @(%{$inbox.Items | where {$_.UnRead}}).Count
+Write-Host $unreadCount
+[array]$ItemCount = @(%{$inbox.Items }).Count
+Write-Host $ItemCount
